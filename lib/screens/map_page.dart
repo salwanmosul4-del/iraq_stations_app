b7ceb48c5465f8b94d6cb7ff343208c22c6b62f4
salwanmosul4-del <salwanmosul4@gmail.com>
2026-09-26 01:37:46 +0300

    Update map_page.dart

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
